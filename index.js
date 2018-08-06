c5bbbd9277894b8fa936afcd5dac51600854500b
jmoypace <jmoypace@gmail.com>
2018-08-06 16:36:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function postComment() {
   var comment = document.getElementById("commentText").value;
   //insert comment into "comments" div in this format:
   //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
- 
+
+  //create template string
   var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  //create template function
+  var templateFn = _.template(commentTemplate);
 }
